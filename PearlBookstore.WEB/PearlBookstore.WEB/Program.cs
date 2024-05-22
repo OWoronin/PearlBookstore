9ff1667605b96ab51c178d811ea8d90f21f5ad81
Michał Żuk <michal.zuk30601@gmail.com>
2024-05-22 20:18:38 +0200

    added docker support for client project

--- PearlBookstore.WEB/PearlBookstore.WEB/Program.cs
+++ PearlBookstore.WEB/PearlBookstore.WEB/Program.cs
@@ -1,25 +1,25 @@
-using PearlBookstore.WEB.Client.Pages;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using PearlBookstore.WEB.Components;
 
 var builder = WebApplication.CreateBuilder(args);
-
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 // Add services to the container.
 builder.Services.AddRazorComponents()
-	.AddInteractiveServerComponents()
-	.AddInteractiveWebAssemblyComponents();
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseWebAssemblyDebugging();
+    app.UseWebAssemblyDebugging();
 }
 else
 {
-	app.UseExceptionHandler("/Error", createScopeForErrors: true);
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -28,8 +28,8 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-	.AddInteractiveServerRenderMode()
-	.AddInteractiveWebAssemblyRenderMode()
-	.AddAdditionalAssemblies(typeof(PearlBookstore.WEB.Client._Imports).Assembly);
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(PearlBookstore.WEB.Client._Imports).Assembly);
 
 app.Run();
